ea085b300f5421aa0ecc4ad546e68ff24eb88687
RavenKMusic <85319728+ravenkmusic@users.noreply.github.com>
2022-01-23 19:32:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+
+from art import logo
+
+print(logo)
+
+name = input("What is your name? ")
+bid_price = input("How much do you wanna bid? $")
+other_bidder = input("Are there other users who would like to bid? Enter yes or no.")
+
+all_bidders = []
+
+def add_bidders_info(bidder, price):
+  bidders_info = {}
+  bidders_info["name"] = bidder
+  bidders_info["bid"] = price
+  
+  all_bidders.append(bidders_info)
+
+add_bidders_info(name, bid_price)
+
+def find_highest_bid():
+  
+
+def others_bidders():
+  if other_bidder == yes:
+    clear()
+  else:
+    find_highest_bid()
